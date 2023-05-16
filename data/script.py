8b2d70fb6ff79bccf2093fb40226ba7e723b74f8
jnskoe <jonsonkoehler@gmail.com>
2023-05-17 01:58:11 +0200

    weather

--- data/script.py
+++ data/script.py
@@ -1,4 +1,8 @@
 import pandas as pd
-df = pd.read_csv("https://raw.githubusercontent.com/od-ms/radverkehr-zaehlstellen/main/100031297/2019-01.csv")
+#df = pd.read_csv("https://raw.githubusercontent.com/od-ms/radverkehr-zaehlstellen/main/100031297/2019-01.csv")
 
-df.to_sql("Monthdata",'sqlite:///data.sqlite')
+#df.to_sql("Monthdata",'sqlite:///data.sqlite')
+
+df = pd.read_csv("/home/wicket/Downloads/stundenwerte_TF_01766_19891001_20221231_hist/produkt_tf_stunde_19891001_20221231_01766.txt",sep=";")
+
+df.to_sql("weather2",'sqlite:///data.sqlite')
