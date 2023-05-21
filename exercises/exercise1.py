f60368f11653605643e4d2d3f1028546b86c53b4
jnskoe <jonsonkoehler@gmail.com>
2023-05-21 03:01:44 +0200

    ex1v2

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -2,4 +2,4 @@ import pandas as pd
 import sqlalchemy as sq
 df = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv",sep=";")
 
-df.to_sql("airports",'sqlite:///data/airports.sqlite',if_exists="replace",dtype={"column_1": sq.INTEGER,"column_2": sq.TEXT})
+df.to_sql("airports",'sqlite:///airports.sqlite',if_exists="replace",dtype={"column_1": sq.INTEGER,"column_2": sq.TEXT})
