ace749bbb1ecb5207c1074b4f877d5707e819534
jnskoe <jonsonkoehler@gmail.com>
2023-06-19 03:09:44 +0200

    ex3v3

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import sqlalchemy as sq
+
+def valint(input):
+    if (type(input) != int):
+        return False
+    return input > 0
+
+def valcin(input):
+    if (type(input) != str):
+        return False
+    return len(input) == 5
+
 fields = range(83)
 df = pd.read_csv("https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv",header=None,sep=';',encoding = "iso-8859-15",names = fields)
 
@@ -17,4 +28,20 @@ df.drop(df.iloc[:, 9:18], inplace=True, axis=1)
 df.drop(df.iloc[:, 10:20], inplace=True, axis=1)
 #df.rename({'0': 'date', '1': 'CIN','2': 'name', '12': 'petrol','22': 'diesel', '32': 'gas','42': 'electro', '52': 'hybrid','62': 'plugInHybrid', '72': 'others'},axis = 1, inplace=True)
 df = df.set_axis(['date', 'CIN','name',  'petrol', 'diesel',  'gas', 'electro',  'hybrid', 'plugInHybrid', 'others'],axis=1)
-df.to_sql("cars",'sqlite:///cars.sqlite',if_exists="replace")
+
+df = df[df["petrol"].apply(lambda x: valint(x))]
+
+df = df[df["CIN"].apply(lambda x: valcin(x))]
+
+df.to_sql("cars",'sqlite:///cars.sqlite',if_exists="replace",index = False, dtype={
+    "date": sq.TEXT,
+    "name": sq.TEXT,
+    "CIN": sq.TEXT,
+    'petrol': sq.BIGINT,
+    'diesel': sq.BIGINT, 
+    'gas': sq.BIGINT,
+    'electro': sq.BIGINT, 
+    'hybrid': sq.BIGINT,
+    'plugInHybrid': sq.BIGINT,
+    'others': sq.BIGINT
+})
